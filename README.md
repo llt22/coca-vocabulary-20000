d85034410eca677bdce9ae70df53ac9cef82e088
262387013@qq.com <li262387013>
2022-02-26 19:01:02 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# coca
+# 项目介绍
+为英语单词收集实用、丰富、令人印象深刻的使用场景，帮助学习好更好的记忆单词
+
+# 背景
+兴趣是最好的老师，一个对《哈利波特》的故事本身都不感兴趣的人，能通过读原版书学习英语吗？一个不喜欢唱歌的人，能通过学习英语歌曲学习英语吗？回想一下，有些事情，我们不用刻意去记，也会深深映入脑子中
+
+比如看过《飞出个未来》的人可能一辈子也不会忘记这个场景
+
+![deterrent](images/shutup.jfif )
+
+比如我前几天看了一个 [reddit帖子](https://www.reddit.com/r/funny/comments/sxhpjo/deterrent) ，`diarrhea` 这个词我感觉自己很难忘记了
+
+![deterrent](images/deterrent.jpg)
+
+
+# COCA 20000 词库介绍
+[如何积累20000词汇量（COCA词频）](https://zhuanlan.zhihu.com/p/20800565)
+
+# 🤝 贡献 | Contributing
+为 20000 个单词收集场景图片是个巨大的工程，还好 [人人词典](https://www.91dict.com/) 已经从影视作品中收集了大量的资源
+
+但我认为是不够的，不经过精心筛选，影视中的很多场景也无法给人留下深刻的印象，因此还需要大量的人工收集
+
+收集来源建议：
+- 搞笑图片：[9gag](https://9gag.com/) [imgur](https://imgur.com/) [reddit](https://www.reddit.com/)
+- stand-up：[米国脱口秀](https://space.bilibili.com/142371069?from=search&seid=9413455629274285401&spm_id_from=333.337.0.0)
+- 情景喜剧：如《生活大爆炸》《老友记》等
+- 其它影视作品
---
+++ index.js
@@ -0,0 +1,41 @@
+const XLSX = require("xlsx");
+const fs = require('fs')
+
+let count = 0
+let fileName
+let writeStream
+
+fs.readdir('excel', (err, files) => {
+    files.forEach(file => {
+        const workbook = XLSX.readFile(`excel/${file}`)
+        const sheetName = workbook.SheetNames[0]
+        const worksheet = workbook.Sheets[sheetName]
+        const items = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+
+        items.forEach((item) => {
+            const word = item[0]
+            const phonetic = item[1]
+            const paraphrase = item[2]
+            if (count % 200 === 0) {
+                fileName = `md/${count + 1}_${count + 200}.md`
+                writeStream = fs.createWriteStream(fileName, {
+                    flags: 'a' // 'a' means appending (old data will be preserved)
+                })
+
+            }
+
+            const r_word = `${count + 1} ${word} \r\n`
+            const r_phonetic = `- ${phonetic} \r\n`
+            const r_paraphrase = `- ${paraphrase} \r\n`
+            const r_d_ren = `- [人人词典](https://www.91dict.com/words?w=${word}) `
+            const r_d_ke = `[柯林斯](https://www.collinsdictionary.com/zh/dictionary/english/${word}) `
+            const r_d_lan = `[朗文](https://www.ldoceonline.com/dictionary/${word}) `
+            const end = '\r\n\r\n'
+
+
+            writeStream.write(r_word + r_phonetic + r_paraphrase + r_d_ren + r_d_ke + r_d_lan + end)
+            count++
+        })
+
+    });
+});
